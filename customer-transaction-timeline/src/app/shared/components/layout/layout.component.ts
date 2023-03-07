0399edbbe3c64efb0337e8aac21d06a8567daaa8
lourens@divotion <lourens.kaufmann@divotion.com>
2023-03-07 07:41:38 +0100

    Added global styling & added layout styling

--- customer-transaction-timeline/src/app/shared/components/layout/layout.component.ts
+++ customer-transaction-timeline/src/app/shared/components/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
+  imports: [RouterModule]
 })
 export class LayoutComponent {
 
